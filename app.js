25006e6899bd09e0b3daa32d0d21e41a30f1731a
Abdulbarikassim <abdicsxhaka10@gmail.com>
2024-11-21 10:21:39 +0400

    update the package.json file

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require("express");
-const connectDB = require("./config/db");
+const { connectDB } = require("./config/db");
 const lessonsRoutes = require("./routes/lessonsRoutes");
+const { logger } = require("./routes/logger");
+//express app and port for the server.
 const app = express();
+const port = process.env.PORT || 5001;
 // connect to mongodb.
 connectDB();
-// middleware.
 
 app.use(express.json());
 
-//routes.
-
+// logger middleware.
+app.use(logger);
+//staic.
+app.use(
+  "/images",
+  express.static(
+    "/Users/abdulbari/Desktop/Uniwork/Fullstack Development/FrontEndcourseWorkProject/images"
+  )
+);
+// routes for API's.
 app.use("/api", lessonsRoutes);
 
-// export the app to server.
-
-module.exports = app;
+app.listen(port, () => {
+  console.log("server running on port: ", port);
+});
